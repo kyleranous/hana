3a95a17eb33129fe46f6fcca6efd8e999eef4902
Kyle Ranous <kranous@Concept32designs.com>
2022-03-25 14:07:47 -0600

    Added unit tests for Swarm and Node models, added functions to models to pull data from docker API

--- hana/swarman/models.py
+++ hana/swarman/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import requests
+import json
 
 # Create your models here.
 class SwarmService(models.Model):
@@ -7,6 +9,201 @@ class SwarmService(models.Model):
     name = models.CharField(max_length=32)
 
 
-class ContainerImage(models.Model):
+class Swarm(models.Model):
+    """
+    Swarm Model is for multiple swarm management. This is a feature that may be implemented at a future time
+    """
+    swarm_name = models.CharField(max_length=64)
+    manager_join_token = models.CharField(max_length=200, blank=True, null=True)
+    worker_join_token = models.CharField(max_length=200, blank=True, null=True)
 
-    name = models.CharField(max_length=200)
+    def __str__(self):
+
+        return f'{self.swarm_name} - {self.node_count()} Node Swarm'
+
+    def manager_join_command(self):
+        '''Returns a String with the docker swarm join command and Manager Token'''
+        return f'docker swarm join --token {self.manager_join_token}'
+
+    def worker_join_command(self):
+        '''Returns a String with the docker swarm join command and Worker Token'''
+        return f'docker swarm join --token {self.worker_join_token}'
+
+    def manager_nodes(self):
+        '''Returns a QuerySet will all manager nodes assigned to a Swarm'''
+        return self.nodes.filter(role='Manager').all()
+
+    def worker_nodes(self):
+        '''Returns a QuerySet with all worker nodes assigned to a Swarm'''
+        return self.nodes.filter(role='Worker').all()
+
+    def node_count(self):
+        '''Returns a count of all nodes assigned to a swarm'''
+        return len(self.nodes.all())
+
+    def manager_count(self):
+        '''Returns a count of all manager nodes assigned to a swarm'''
+        return len(self.manager_nodes())
+
+    def worker_count(self):
+        '''Returns a count of all worker nodes assigned to a swarm'''
+        return len(self.worker_nodes())
+
+    def manager_ip_list(self):
+        '''Return a list of Manager Addresses'''
+        managers = self.manager_nodes()
+        addresses = []
+        for manager in managers:
+            addresses.append(f'{manager.ip_address}:{manager.api_port}')
+
+        return addresses
+
+
+class Node(models.Model):
+    """
+    
+    """
+    hostname = models.CharField(max_length=64)
+    ip_address = models.GenericIPAddressField(blank=True, null=True, unique=True)
+    api_port = models.CharField(max_length=5)
+    role = models.CharField(max_length=16)
+    swarm = models.ForeignKey(Swarm, on_delete=models.CASCADE, related_name="nodes")
+    docker_version_index = models.CharField(max_length=16)
+    node_architecture = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.hostname
+
+    def promote(self):
+        '''
+            Promotes a node from worker to manager
+            docker API v1.41    
+        '''
+        if self.role == "Manager":
+            return "Node is already a Manager"
+
+        else:
+            # Attempt to update, if communication fails,
+            # Go to the next manager in the list
+            for address in self.swarm.manager_ip_list():
+                url = f'http://{address}/nodes/{self.hostname}/update?version={self.docker_version_index}'
+                
+                # post Request to swarm to promote Manager
+                request_body = {
+                    "Role": "manager"
+                }
+                # If Request is Successful - Update Role in database
+                self.role = "Manager"
+                self.save()
+                return "Update Successful"
+
+    def demote(self):
+        '''
+            Demotes a node from a manager to a worker
+            docker API v1.41
+        '''
+        if self.role == "Worker":
+            return "Node is already a Worker"
+
+        else:
+            # Attempt to update, if communication fails,
+            # Go to the next manager in the list
+            for address in self.swarm.manager_ip_list():
+                url = f'http://{address}/nodes/{self.hostname}/update?version={self.docker_version_index}'
+                
+                # post Request to swarm to promote Manager
+                request_body = {
+                    "Role": "worker"
+                }
+                # If Request is Successful - Update Role in database
+                self.role = "Worker"
+                self.save()
+                return "Update Successful"
+
+    def get_node_info(self):
+        '''
+            Returns a JSON object with node information from Docker API
+            docker API v1.41    
+        '''
+
+        # Get a list of all manager IPs, If a manager does not respond
+        # Try the next manager
+        for address in self.swarm.manager_ip_list():
+            url = f'http://{address}/nodes/{self.hostname}'
+            node_response = requests.get(url)
+
+            if node_response.status_code == '200':
+                node_data = node_response.text
+                
+                return json.loads(node_data)
+
+            else:
+                pass
+
+        return None
+
+    def get_cpu_load(self):
+        '''
+            Calculate the current CPU load on the node, returns value as a percentage
+            docker API v1.41
+        '''
+
+        container_response = requests.get(f'http://{self.ip_address}:{self.api_port}/containers/json')
+        container_json = json.loads(container_response.text)
+
+        total_cpu_load = 0
+
+        for container in container_json:
+            id = container['Id']
+            container_info = self.get_container_info(id)
+
+            cpu_delta = container_info['cpu_stats']['cpu_usage']['total_usage'] - container_info['precpu_stats']['cpu_usage']['total_usage']
+            system_cpu_delta = container_info['cpu_stats']['system_cpu_usage'] - container_info['precpu_stats']['system_cpu_usage']
+            number_cpus = container_info['cpu_stats']['online_cpus']
+            cpu_usage = (cpu_delta / system_cpu_delta) * number_cpus * 100.0
+            total_cpu_load += cpu_usage
+
+        return total_cpu_load
+
+
+    def get_container_info(self, container_id):
+        '''
+            Get the container info from a node
+            docker API v1.41    
+        '''
+        
+        container_response = requests.get(f'http://{self.ip_address}:{self.api_port}/containers/{container_id}/stats?stream=0')
+        container_json = json.loads(container_response)
+
+        return container_json
+        
+
+    def get_memory_usage(self, container_id):
+        '''
+            Calculate Memory Usage on a Node, returns memory usage as percentage
+            docker API v1.41
+        '''
+        # Check to see if node is armv71
+        if self.node_architecture == "armv7l":
+            return "Operation not supported on armv71 architecture"
+        # Otherwise calculate memory usage on node
+        else:
+
+            container_response = requests.get(f'http://{self.ip_address}:{self.api_port}/containers/json')
+            container_json = json.loads(container_response.text)
+
+            total_memory_load = 0
+            # Loop through all containers returned in container JSON
+            # and calculate memory usage
+            for container in container_json:
+
+                id = container['Id']
+                container_info = self.get_container_info(id)
+
+                used_memory = container_info['memory_stats']['usage'] - container_info['memory_stats']['stats']['cache']
+                available_memory = container_info['memory_stats']['limit']
+                memory_usage = (used_memory / available_memory) * 100.0
+                total_memory_load += memory_usage
+
+            return total_memory_load
+
--- hana/swarman/tests.py
+++ hana/swarman/tests.py
@@ -1,3 +1,101 @@
 from django.test import TestCase
 
+from .models import Swarm, Node
+
+
 # Create your tests here.
+class SwarmModelTests(TestCase):
+
+    def setUp(self):
+        self.swarm = Swarm.objects.create(swarm_name="Test Swarm",
+                                          manager_join_token="TEst-MaNgEr-ToKeN",
+                                          worker_join_token="TEst-WoRkEr-ToKeN")
+        self.node1 = Node.objects.create(hostname="testnode1",
+                                         ip_address="0.0.0.0",
+                                         api_port="2375",
+                                         role="Manager", swarm=self.swarm)
+        self.node2 = Node.objects.create(hostname="testnode2",
+                                         ip_address="0.0.0.1",
+                                         api_port="2375",
+                                         role="Manager", swarm=self.swarm)
+        self.node3 = Node.objects.create(hostname="testnode3",
+                                         ip_address="0.0.0.2",
+                                         api_port="2375",
+                                         role="Worker", swarm=self.swarm)
+        self.node4 = Node.objects.create(hostname="testnode4",
+                                         ip_address="0.0.0.3",
+                                         api_port="2375",
+                                         role="Worker", swarm=self.swarm)
+
+    def test_swarm_join_token_commands(self):
+        '''Test that swarm returns correct docker join commands for worker and manager'''
+        self.assertEqual(self.swarm.manager_join_command(), 'docker swarm join --token TEst-MaNgEr-ToKeN')
+        self.assertEqual(self.swarm.worker_join_command(), 'docker swarm join --token TEst-WoRkEr-ToKeN')
+
+    def test_swarm_manager_nodes(self):
+        '''Test that manager_nodes() returns all manager nodes belonging to a swarm'''
+        self.assertEqual(list(self.swarm.manager_nodes()), [self.node1, self.node2])
+
+    def test_swarm_worker_nodes(self):
+        '''Test that worker_nodes() returns all worker nodes belonging to a swarm'''
+        self.assertEqual(list(self.swarm.worker_nodes()), [self.node3, self.node4])
+
+    def test_swarm_node_count(self):
+        '''Test that node_count returns a count of all Nodes in a swarm'''
+        self.assertEqual(self.swarm.node_count(), 4)
+
+    def test_swarm_node_manager_count(self):
+        '''Test the manager_count() returns a count of all Manager Nodes in a swarm'''
+        self.assertEqual(self.swarm.manager_count(), 2)
+
+    def test_swarm_node_worker_count(self):
+        '''Test the worker_count() returns a count of all Manager Nodes in a Swarm'''
+        self.assertEqual(self.swarm.worker_count(), 2)
+
+    def test_manager_ip_list(self):
+        '''
+            Test that manager_ip_list returns a formatted [IP ADDRESS]:[PORT] list of 
+            all managers assigned to the swarm
+        '''
+        address_list = [
+            f'{self.node1.ip_address}:{self.node1.api_port}', 
+            f'{self.node2.ip_address}:{self.node2.api_port}'
+        ]
+
+        self.assertEqual(self.swarm.manager_ip_list(), address_list)
+
+
+class NodeModelTests(TestCase):
+
+    def setUp(self):
+        self.swarm = Swarm.objects.create(swarm_name="Test Swarm",
+                                          manager_join_token="TEst-MaNgEr-ToKeN",
+                                          worker_join_token="TEst-WoRkEr-ToKeN")
+        self.node1 = Node.objects.create(hostname="testnode1",
+                                         ip_address="0.0.0.0",
+                                         api_port="2375",
+                                         role="Manager", swarm=self.swarm)
+        self.node2 = Node.objects.create(hostname="testnode2",
+                                         ip_address="0.0.0.1",
+                                         api_port="2375",
+                                         role="Manager", swarm=self.swarm)
+        self.node3 = Node.objects.create(hostname="testnode3",
+                                         ip_address="0.0.0.2",
+                                         api_port="2375",
+                                         role="Worker", swarm=self.swarm)
+        self.node4 = Node.objects.create(hostname="testnode4",
+                                         ip_address="0.0.0.3",
+                                         api_port="2375",
+                                         role="Worker", swarm=self.swarm)
+
+    def test_node_promote(self):
+        '''Test that test_node_promote() will successfully Promote a worker to a manager'''
+        self.assertEqual(self.node1.promote(), "Node is already a Manager")
+        self.assertEqual(self.node3.promote(), "Update Successful")
+        self.assertEqual(self.node3.role, "Manager")
+
+    def test_node_demote(self):
+        '''Test that test_node_demote() will successfully demote a manager to a worker'''
+        self.assertEqual(self.node3.demote(), "Node is already a Worker")
+        self.assertEqual(self.node1.demote(), "Update Successful")
+        self.assertEqual(self.node1.role, "Worker")
