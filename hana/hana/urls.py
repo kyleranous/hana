b6abfca0a3e42d8229fee830f0a0f2fef4760759
Kyle Ranous <kranous05@gmail.com>
2022-03-26 23:59:28 -0600

    Added namespace 'swarman' to swarman app

--- hana/hana/urls.py
+++ hana/hana/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swarman/', include('swarman.urls'))
+    path('swarman/', include(('swarman.urls', 'swarman'), namespace='swarman'))
 ]
