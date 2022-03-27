5635424876c18a45b467178cd525906157798f8d
Kyle Ranous <kranous05@gmail.com>
2022-03-27 00:04:11 -0600

    Added new_swarm and create_swarm endpoints

--- hana/swarman/urls.py
+++ hana/swarman/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('nodes/<str:node_id>', views.node_detail, name='node_detail'),
+    path('new_swarm/', views.new_swarm, name='new_swarm'),
+    path('create_swarm/', views.create_swarm, name='create_swarm')
 ]
