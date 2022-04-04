6770604c06b8d3db25da900f6866689bb4773307
Kyle Ranous <kranous05@gmail.com>
2022-04-04 09:45:37 -0600

    Added an index view

--- hana/hana/urls.py
+++ hana/hana/urls.py
@@ -19,6 +19,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from . import views
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,6 +34,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('swarman/', include(('swarman.urls', 'swarman'), namespace='swarman')),
     path('docs/api', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
