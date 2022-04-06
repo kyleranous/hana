23c4bc5367b4dd5c4245ce85e1489f6295d27ad0
Kyle Ranous <kranous05@gmail.com>
2022-04-06 10:41:25 -0600

    Fixed URL names to be kabab-case, adjusted multi imports to use parenthesis notation

--- hana/swarman/urls.py
+++ hana/swarman/urls.py
@@ -1,15 +1,17 @@
-from django.urls import path, include
+from django.urls import (
+    path,
+    include,
+)
 
 from . import views
-
 
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/<int:swarm_id>', views.swarm_dashboard, name='swarm-dashboard'),
-    path('nodes/<str:node_id>', views.node_detail, name='node_detail'),
-    path('new_swarm/', views.AddSwarm.as_view(), name='new_swarm'),
-    path('create_swarm/', views.create_swarm, name='create_swarm'),
+    path('nodes/<str:node_id>', views.node_detail, name='node-detail'),
+    path('new_swarm/', views.AddSwarm.as_view(), name='new-swarm'),
+    path('create_swarm/', views.create_swarm, name='create-swarm'),
     path('api/', include('swarman.api.urls')),
 
 ]
