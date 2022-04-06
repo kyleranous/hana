87bb034893619ae5bdafca65b77a8036b671bde0
Kyle Ranous <kranous05@gmail.com>
2022-04-06 10:52:04 -0600

    Broke long line into 2 lines

--- hana/swarman/urls.py
+++ hana/swarman/urls.py
@@ -8,7 +8,8 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/<int:swarm_id>', views.swarm_dashboard, name='swarm-dashboard'),
+    path('dashboard/<int:swarm_id>', views.swarm_dashboard,
+         name='swarm-dashboard'),
     path('nodes/<str:node_id>', views.node_detail, name='node-detail'),
     path('new_swarm/', views.AddSwarm.as_view(), name='new-swarm'),
     path('create_swarm/', views.create_swarm, name='create-swarm'),
