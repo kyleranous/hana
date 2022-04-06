b514ab4ac5b87fe79e07f79871da22d55079bd10
Kyle Ranous <kranous05@gmail.com>
2022-04-06 10:35:17 -0600

    Cleaned up Imports, Reformatted imports to use paranthesis notation, corrected URL names to kabob-case prepended with 'api'

--- hana/swarman/api/v1_0/urls.py
+++ hana/swarman/api/v1_0/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 
-from .api_views import SwarmViewSet, NodeList, get_existing_swarm_nodes, add_existing_swarm_nodes, leave_swarm
-from .api_views import promote_node, demote_node, node_utilization
+from .api_views import (
+    SwarmViewSet,
+    NodeList,
+    get_existing_swarm_nodes,
+    add_existing_swarm_nodes,
+    leave_swarm,
+)
+from .api_views import (
+    promote_node,
+    demote_node,
+    node_utilization,
+)
 
 
 swarm_list = SwarmViewSet.as_view({
@@ -17,12 +27,16 @@ swarm_detail = SwarmViewSet.as_view({
 urlpatterns = [
     path('swarms/', swarm_list, name='api-swarm-list'),
     path('swarms/<int:pk>', swarm_detail, name='api-swarm-detail'),
-    path('swarms/<int:swarm_id>/nodes', NodeList.as_view(), name='api-node-list'),
-    path('swarms/existing_swarm_nodes', get_existing_swarm_nodes, name="existing-swarm-nodes"),
-    path('swarms/add_existing_swarm_nodes', add_existing_swarm_nodes, name="api-add-swarm-nodes"),
+    path('swarms/<int:swarm_id>/nodes', NodeList.as_view(),
+         name='api-node-list'),
+    path('swarms/existing_swarm_nodes', get_existing_swarm_nodes,
+         name="api-existing-swarm-nodes"),
+    path('swarms/add_existing_swarm_nodes',
+         add_existing_swarm_nodes, name="api-add-swarm-nodes"),
 
     path('nodes/<int:node_id>/leave', leave_swarm, name='api-leave-swarm'),
     path('nodes/<int:node_id>/promote', promote_node, name='api-node-promote'),
     path('nodes/<int:node_id>/demote', demote_node, name='api-node-demote'),
-    path('nodes/<int:node_id>/utilization', node_utilization, name='api-node-utilization')
+    path('nodes/<int:node_id>/utilization',
+         node_utilization, name='api-node-utilization')
 ]
