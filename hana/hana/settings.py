b40a3a6ebabdb946fb5309ebd8d0ba1b053f2dbe
Kyle Ranous <kranous05@gmail.com>
2022-04-11 11:24:35 -0600

    Moved SECRET_KEY to .env file which is part of gitignore

--- hana/hana/settings.py
+++ hana/hana/settings.py
@@ -10,9 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 
+# Ubutuakuze environment variables
+
+env = environ.Env()
+
+environ.Env.read_env() 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ddj5%69ze*w9n0yy*$suju@*7v3t(+(mt=$^!1k8xcu$1c(y0g'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
