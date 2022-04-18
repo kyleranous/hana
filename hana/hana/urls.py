75c80d0e7efd7b173990bf7442f2939ec443e3a9
Kyle Ranous <kranous05@gmail.com>
2022-04-17 19:46:05 -0600

    Added graphql endpoint

--- hana/hana/urls.py
+++ hana/hana/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from graphene_django import GraphQLView
 
 from . import views
 
@@ -38,5 +39,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swarman/', include(('swarman.urls', 'swarman'), namespace='swarman')),
     path('docs/api', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
 
