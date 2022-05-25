bdc951afcdca8010a86d2ad1e7d08ee827188c70
Kyle Ranous <kranous05@gmail.com>
2022-05-24 21:41:01 -0600

    changed ./ back to source, change from sh to bash

--- envsetup.sh
+++ envsetup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -d "env" ]
 then
@@ -7,7 +7,7 @@ else
     python3 -m venv venv
 fi
 
-./venv/bin/activate
+source /venv/bin/activate
 
 
 pip3 install -r requirements.txt
