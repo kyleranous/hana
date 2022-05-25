a554f93c89289ebcddcd61366c14b6be9a5806d7
Kyle Ranous <kranous05@gmail.com>
2022-05-25 08:05:22 -0600

    Adjusted nginx setup script

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo cp -rf app.conf /etc/nginx/conf.daemon
+sudo cp app.conf /etc/nginx/sites-enabled
+
 sudo usermod -aG jenkins nginx
 chmod 710 /var/lib/jenkins/workspace/HANA-TEST
 
